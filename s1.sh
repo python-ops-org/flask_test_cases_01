b998f503683678dfab49cf46bea339a91d2784bf
nik7864 <chakraborty.rock@gmail.com>
2023-08-22 21:27:20 +0530

    jira-05

---
+++ s1.sh
@@ -0,0 +1,7 @@
+sonar-scanner \
+  -Dsonar.projectKey=sample_test_03 \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://127.0.0.1:9000 \
+  -Dsonar.token=sqp_44ca05bbb6fe8b31f315c2221b28ba10084b609b \
+  -Dsonar.projectBaseDir=/home/nik/Desktop/k8s/python/python/sonar/sample_03
+
